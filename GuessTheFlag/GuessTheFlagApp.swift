6bfa4dc2a8ad259a9bce8b70bd4061ba9a99ec82
Francisco J. Casillas <chiccasillas@gmail.com>
2024-11-07 11:27:07 -0600

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Francisco Jean on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
